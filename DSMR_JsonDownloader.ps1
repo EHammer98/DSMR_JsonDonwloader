93d217c4400c4e5194c326eecb377838b4c28df8
Elwin Hammer <elwinhammer@gmail.com>
2021-02-17 12:21:58 +0100

    Frist version fot he DSMR JsonDownloader

---
+++ DSMR_JsonDownloader.ps1
@@ -0,0 +1,14 @@
+﻿while($true){
+$url = "http://<IP_OF_ESP32>/getActual.json"
+$output = "\\<HASSIO_IP>\share\DSMR-Actual.json"
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+#OR
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+Write-Host "Loop is klaar"
+#604800 ; 7dagen
+#86400 ; 1dag 
+#3600 ; 1uur
+    Start-Sleep -s 60
+}
